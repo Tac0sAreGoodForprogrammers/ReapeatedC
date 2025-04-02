f5c30ff1b42ffc1da75100e55ee8b8c2e54c3ceb
Tac0sAreGoodForprogrammers <sknatsko@gmail.com>
2025-04-02 18:53:01 -0400

    added main.c

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    // Empty main function
+    int n = 100;  // Pre-set value of n
+
+    // Loop through numbers from 1 to n
+    for (int i = 1; i <= n; i++) {
+        // Check if the number is even or odd
+        if (i % 2 == 0) {
+            printf("%d is even\n", i);
+        } else {
+            printf("%d is odd\n", i);
+        }
+    }
+
     return 0;
 }

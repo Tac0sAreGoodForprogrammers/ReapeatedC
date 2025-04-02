3ead36ed1815ef7939c4b6eab285c19a779c9e29
Tac0sAreGoodForprogrammers <sknatsko@gmail.com>
2025-04-02 18:58:01 -0400

    hehe funny factors

--- main.c
+++ main.c
@@ -1,26 +1,50 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdbool.h>
+
+#define MAX 1000000
+
+// Sieve of Eratosthenes to generate prime numbers up to a limit
+void sieve(int primes[], int limit) {
+    bool is_prime[limit + 1];
+    for (int i = 0; i <= limit; i++) is_prime[i] = true;
+    is_prime[0] = is_prime[1] = false;
+    
+    for (int i = 2; i * i <= limit; i++) {
+        if (is_prime[i]) {
+            for (int j = i * i; j <= limit; j += i) {
+                is_prime[j] = false;
+            }
+        }
+    }
+    
+    // Store primes in the primes array
+    int idx = 0;
+    for (int i = 2; i <= limit; i++) {
+        if (is_prime[i]) {
+            primes[idx++] = i;
+        }
+    }
+}
 
 void prime_factors(int n) {
     printf("Prime factors of %d: ", n);
     
-    // Divide out all 2s
-    while (n % 2 == 0) {
-        printf("%d ", 2);
-        n = n / 2;
-    }
-    
-    // Check odd numbers from 3 upwards, only up to sqrt(n)
+    // Generate primes up to sqrt(n)
     int limit = (int)sqrt(n);
-    for (int i = 3; i <= limit; i += 2) {
-        while (n % i == 0) {
-            printf("%d ", i);
-            n = n / i;
+    int primes[MAX];
+    sieve(primes, limit);
+    
+    // Factorize using the primes
+    for (int i = 0; primes[i] <= limit && primes[i] <= n; i++) {
+        while (n % primes[i] == 0) {
+            printf("%d ", primes[i]);
+            n = n / primes[i];
         }
     }
     
-    // If n is a prime number greater than 2
-    if (n > 2) {
+    // If n is a prime number greater than the limit
+    if (n > 1) {
         printf("%d ", n);
     }
     
